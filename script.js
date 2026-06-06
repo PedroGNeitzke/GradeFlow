b7615b22e944a641c21378dece5a08c9faf56b6c
Pedro Neitzke <pgn.neitzke@gmail.com>
2026-06-06 02:12:24 -0300

    feat: dashboar layout and cards

--- script.js
+++ script.js
@@ -6,3 +6,40 @@ const dropdown = document.querySelector('.dropdown');
 profileBtn.addEventListener('click', () => {
     dropdown.classList.toggle('active');
 });
+
+const ctx = document.getElementById('situationchart');
+
+new Chart(ctx, {
+    type: 'doughnut',
+
+    data: {
+        labels: [
+            'Aprovadas',
+            'Melhorar',
+            'Críticas'
+        ],
+
+        datasets: [{
+            data: [10, 2, 4],
+
+            backgroundColor: [
+                '#22C55E',
+                '#F59E0B',
+                '#EF4444'
+            ],
+
+            borderWidth: 0
+        }]
+    },
+
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
